b6a15c2b30ebe5ffbf9e0e8ec03492f186925869
brightsniper99 <francissamuelmendoza7@gmail.com>
2020-01-11 21:14:28 -0700

    FILE: Added average-work.js file. Responsible for timestamp & difficulty calculation

---
+++ average-work.js
@@ -0,0 +1,30 @@
+const Blockchain = require('./blockchain');
+const blockchain = new Blockchain();
+
+blockchain.addBlock({ data: 'initial' });
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average;
+
+//Keep track of timestamps in array
+const times = [];
+
+//Upper limit of 10,000 Proof-Of-Work Blocks
+for (let i = 0; i < 10000; i++) {
+    //Get timestamp of previous block
+    prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp;
+
+    //Iteratively adds blocks
+    blockchain.addBlock({ data: `block ${i}` });
+
+    //Why is it called next block when it refers to block most recently appended to the chain?
+    nextBlock = blockchain.chain[blockchain.chain.length - 1];
+    nextTimestamp = nextBlock.timestamp;
+
+    timeDiff = nextTimestamp - prevTimestamp;
+    times.push(timeDiff);
+
+    //Callback helps calculate average by appending number to growing total
+    average = times.reduce((total, num) => (total + num)) / times.length;
+
+    console.log(`Time to mine block: ${timeDiff} ms. Difficulty: ${nextBlock.difficulty}. Average Time: ${average} ms.`);
+}
