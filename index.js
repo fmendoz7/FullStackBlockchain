1bbed455501b2c56e292cd41c3c671397d50ffb1
brightsniper99 <francissamuelmendoza7@gmail.com>
2020-01-12 21:48:56 -0700

    FUNCTIONALITY: Added POST APIs for Block data. Will add pubsub functionality in the future for scalability

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
+    //creating new instance of 'Blockchain'
+
+app.use(bodyParser.json());
 
 //GET HTTP requests are designed to read data from the backend
 app.get('/api/blocks', (req, res) => {
@@ -10,6 +15,9 @@ app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
 });
 
+//(!!) Cannot GET /api/mine, because we only have get for /api/blocks
+
+//User is going to receive data in JSON format 
 app.post('/api/mine', (req, res) => {
     const { data } = req.body;
 
