4e2c0c444eb360e3cd14c6f75b5cd3d377e722f1
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-14 23:59:15 -0700

    Problem solved. Asynchronous comms & older version housed by redis ubuntu subsystem

--- flagFile.js
+++ flagFile.js
@@ -1,1 +1,4 @@
 //Just a totally useless file to check version control between two different OS instances 
+
+//HOLY FUCK IT WORKED!
+    //Problem solved. It was just delay of update on Ubuntu subsystem housing redis!!
