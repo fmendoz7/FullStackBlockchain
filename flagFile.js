e5b67053a822dfafaa3e0c7c58d58884d7d65a7a
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-14 23:59:41 -0700

    FILE: Removed flagFile. Absolutely unnecessary now

--- flagFile.js
+++
@@ -1,4 +0,0 @@
-//Just a totally useless file to check version control between two different OS instances 
-
-//HOLY FUCK IT WORKED!
-    //Problem solved. It was just delay of update on Ubuntu subsystem housing redis!!
