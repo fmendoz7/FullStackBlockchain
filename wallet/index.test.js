1b0d0fe310533bbd2279252bc378ff912f3ad5c7
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-15 23:58:21 -0700

    FUNCTIONALITY: updated wallet unit tests within index.test.js, to ensure signatures signed by a different wallet instance DO NOT get signed

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const {verifySignature} = require('../util');
 
 describe('Wallet', () => {
     let wallet;
@@ -15,4 +16,31 @@ describe('Wallet', () => {
     it('has a `publicKey`', () => {
         expect(wallet).toHaveProperty('publicKey');
     });
+
+    describe('signing data', () => {
+        const data = 'foobar';
+
+        //Valid signature test due to being signed by original wallet
+        it('verifies a signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: wallet.sign(data)
+                })
+            ).toBe(true);
+        });
+
+        //Invalid signature test due to different wallet than original
+        it('does not verify an invalid signature', () => {
+            expect(
+                verifySignature({
+                    publicKey: wallet.publicKey,
+                    data,
+                    signature: new Wallet().sign(data)
+                        //Signature occurred from a DIFFERENT wallet
+                })
+            ).toBe(false);
+        });
+    })
 });
