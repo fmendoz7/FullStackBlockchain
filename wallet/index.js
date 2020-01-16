08e1d8343c95b349620dfe61fecd1820a23b8e99
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-16 00:58:49 -0700

    FUNCTIONALITY: Added additional unit tests & functionality within util index.js file to officially validate correct signatures & invalidate incorrect signatures

--- wallet/index.js
+++ wallet/index.js
@@ -1,13 +1,19 @@
 const {STARTING_BALANCE } = require('../config');
+const cryptoHash = require('../util/crypto-hash');
 const {ec} = require('../util');
 
 class Wallet {
     constructor() {
         this.balance = STARTING_BALANCE;
 
-        const keyPair = ec.genKeyPair();
+        this.keyPair = ec.genKeyPair();
 
-        this.publicKey = keyPair.getPublic().encode('hex');
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
+
+    sign(data) {
+        //Ensures we are signing on an optimized hash version of the data
+        return this.keyPair.sign(cryptoHash(data));
     }
 };
 
