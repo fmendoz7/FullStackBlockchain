c0cde4219f6b695fb80f7090a85d4c4ae0753e81
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-17 23:54:01 -0700

    FUNCTIONALITY: Added createTransaction method within index.js and functionality to throw errors

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,8 @@
 const {STARTING_BALANCE } = require('../config');
 const {ec, cryptoHash} = require('../util');
 
+const Transaction = require('./transaction');
+
 class Wallet {
     constructor() {
         this.balance = STARTING_BALANCE;
@@ -13,6 +15,15 @@ class Wallet {
     sign(data) {
         //Ensures we are signing on an optimized hash version of the data
         return this.keyPair.sign(cryptoHash(data));
+    }
+
+    createTransaction({recipient, amount}) {
+        if(amount > this.balance) {
+            throw new Error('ERROR: Amount exceeds balance');
+        }
+
+        //`: this` shorthand to designate local senderWallet instance
+        return new Transaction({senderWallet: this, recipient, amount});
     }
 };
 
