937d60955127e013ccfc88bae4e30ab1f2f29a6f
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-17 23:47:15 -0700

    FUNCTIONALITY: Adding tests to index.test.js so wallet objects are created automatically

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,4 +1,5 @@
 const Wallet = require('./index');
+const Transaction = require('./transaction');
 const {verifySignature} = require('../util');
 
 describe('Wallet', () => {
@@ -41,6 +42,37 @@ describe('Wallet', () => {
                         //Signature occurred from a DIFFERENT wallet
                 })
             ).toBe(false);
+        });
+    })
+
+    describe('createTransaction()', () => {
+        describe('and the amount exceeds the balance', () => {
+            it('throws an error', () => {
+                expect(() => wallet.createTransaction({amount: 99999, recipient: 'foo-recipient'}))
+                    .toThrow('Amount exceeds balance');
+            });
+        });
+
+        describe('and the amount is valid', () => {
+            let transaction, amount, recipient;
+
+            beforeEach(() => {
+                amount = 50;
+                recipient = 'foo-recipient';
+                transaction = wallet.createTransaction({amount, recipient});
+            });
+            
+            it('creates an instance of `Transaction`', () => {
+                expect(transaction instanceof Transaction).toBe(true);
+            });
+
+            it('matches the transaction input with the wallet', () => {
+                
+            });
+
+            it('outputs the amount to the recipient', () => {
+                
+            });
         });
     })
 });
