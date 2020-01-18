c63ebc490f0d20dab7a78610ad781ccb2ae03bcc
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-17 23:37:48 -0700

    FUNCTIONALITY: Fixed and developed on top of existing unit tests to accept valid transactions and reject invalid ones

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v1');
+const {verifySignature} = require('../util');
 
 class Transaction {
     constructor ({senderWallet, recipient, amount}) {
@@ -26,6 +27,26 @@ class Transaction {
     }
 
     static validTransaction(transaction) {
+        //: notation basically defines what the input is right away
+        //Allows you to define input if you are storing consecutive variables 
+        const {input: {address, amount, signature}, outputMap} = transaction;
+
+        const outputTotal = Object.values(outputMap)
+            .reduce((total, outputAmount) => total + outputAmount);
+
+        //Check if input amount is contained within all values of outputMap
+        //Checks for invalid transactions
+        if(amount !== outputTotal) {
+            console.error(`Invalid Transaction from ${address}`);
+            return false;
+        }
+
+        //Checks for invalid signature 
+        if(!verifySignature({publicKey: address, data: outputMap, signature})) {
+            console.error(`Invalid Signature from ${address}`);
+            return false;
+        }
+
         return true;
     }
 }
