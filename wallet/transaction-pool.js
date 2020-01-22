15b606ad686d692f2efe3b152baf6863a951d313
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-01-21 23:42:44 -0700

    FUNCTIONALITY: Added 'existingTransaction' factory method to return list of transactions from specified user address

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -6,9 +6,16 @@ class TransactionPool {
         };
     }
 
-    //setTransaction method sets value to transactionMap
+    //setTransaction factory method sets value to transactionMap
     setTransaction(transaction) {
         this.transactionMap[transaction.id] = transaction;
+    }
+
+    //existingTransaction factory method returns list of transactions from specified address
+    existingTransaction({inputAddress}) {
+        const transactions = Object.values(this.transactionMap);
+
+        return transactions.find(transaction => transaction.input.address === inputAddress);
     }
 }
 
