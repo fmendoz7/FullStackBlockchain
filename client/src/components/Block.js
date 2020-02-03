199edc2901d85cc3e58888af7decf7add91b7aac
fmendoz7 <francissamuelmendoza7@gmail.com>
2020-02-02 23:56:34 -0700

    FRONTEND: Display truncated hash

--- client/src/components/Block.js
+++ client/src/components/Block.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class Block extends Component {
     render() {
-
+        const {timestamp, hash, data} = this.props.block;
+    
+        const hashDisplay = `${hash.substring(0,15)}...`;
     }
 };
+
+export default Block;
